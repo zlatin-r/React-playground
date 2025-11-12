d874cc55c5c8dbdc0b112c202542c9f4e044387d
zlatin-r <zlatinrusev@gmail.com>
2025-11-12 20:02:08 +0200

    commit

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 const Card = ({title}) => {
     return (
-        <div>
+        <div className="card">
             <h2>{title}</h2>
         </div>
     )
@@ -10,9 +10,7 @@ const Card = ({title}) => {
 
 const App = () => {
     return (
-        <div>
-            <h2> Functional Arrow Component</h2>
-
+        <div className="card-container">
             <Card title="Star Wars"/>
             <Card title="Avatar"/>
             <Card title="Lion King"/>
