2572c42ec243ceb57e114bd281e866ce07eee414
zlatin-r <zlatinrusev@gmail.com>
2025-11-12 18:59:04 +0200

    commit

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -4,11 +4,9 @@ import Quiz from './components/quiz.jsx';
 function App() {
   return (
     <div className="app-container"> 
-
       <h1>Quiz App</h1> 
       
       <Quiz />
-
 
     </div>
   );
