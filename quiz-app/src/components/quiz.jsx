c4d7601246813ec2d6367b34c1216c419239fa6e
zlatin-r <zlatinrusev@gmail.com>
2025-11-12 18:58:50 +0200

    commit

---
+++ quiz-app/src/components/quiz.jsx
@@ -0,0 +1,23 @@
+function Quiz() {
+    const questionsBank = [
+        {
+            question: "What is the capital of France?",
+            options: ["Berlin", "Madrid", "Paris", "Rome"],
+            answer: "Paris",
+        },
+            {
+            question: "Which language is used for web apps?",
+            options: ["Python", "JavaScript", "PHP", "All"],
+            answer: "All",
+        },
+           {
+            question: "What does JSX stand for?",
+            options: ["JavaScript XML", "Java Syntax eXtension", "Java Source eXtension", "None"],
+            answer: "JavaScript XML",
+        },
+    ];
+
+    return <div> Quiz</div>; 
+}
+
+export default Quiz;
