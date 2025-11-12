1d40926f7226c6f6e15ed5e274dcd1926c17706d
zlatin-r <zlatinrusev@gmail.com>
2025-11-12 20:26:03 +0200

    commit

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,17 +1,28 @@
-import './App.css'
+import {useEffect, useState} from "react";
 
 const Card = ({title}) => {
+    const [count, setCount] = useState(0);
+    const [hasLiked, setHasLiked] = useState(false);
+
+    useEffect(() => {
+        console.log(`${title} has been liked: ${hasLiked}`);
+    }, [hasLiked]);
+
     return (
-        <div className="card">
-            <h2>{title}</h2>
+        <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
+            <h2>{title} <br/> {count}</h2>
+            <button onClick={() => setHasLiked(!hasLiked)}>
+                {hasLiked ? '❤️' : '🤍'}
+            </button>
         </div>
     )
 }
 
 const App = () => {
+
     return (
         <div className="card-container">
-            <Card title="Star Wars"/>
+            <Card title="Star Wars" rating={5} isCool={true}/>
             <Card title="Avatar"/>
             <Card title="Lion King"/>
         </div>
