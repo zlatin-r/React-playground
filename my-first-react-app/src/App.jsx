b2b0f848b821413b869a0f605708f3f3f8563c22
zlatin-r <zlatinrusev@gmail.com>
2025-11-23 10:31:04 +0200

    commit

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,8 +1,13 @@
-import Header from './Header.jsx'
+import Card from "./Card.jsx"
 
 function App() {
     return (
-        <Header/>
+        <>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </>
     );
 }
 
