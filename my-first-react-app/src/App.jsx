5fe082fca5a65fdd51fb81d27a0397b1a60affbb
zlatin-r <zlatinrusev@gmail.com>
2025-11-23 10:41:56 +0200

    commit

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,11 @@
 import Card from "./Card.jsx"
+import Button from "./Button/Button.jsx"
 
 function App() {
     return (
         <>
             <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Button/>
         </>
     );
 }
---
+++ my-first-react-app/src/Button/Button.jsx
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css"
+
+function Button() {
+    return (
+        <button className={styles.button}>Click Me</button>
+    );
+}
+
+export default Button;
