13db5198dc8b1106ecc2b80d426f68e0fa4412df
zlatin-r <zlatinrusev@gmail.com>
2025-11-23 09:52:35 +0200

    commit

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,32 +1,9 @@
-import {useEffect, useState} from "react";
+import Header from './Header.jsx'
 
-const Card = ({title}) => {
-    const [count, setCount] = useState(0);
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`);
-    }, [hasLiked]);
-
+function App() {
     return (
-        <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
-            <h2>{title} <br/> {count || null}</h2>
-            <button onClick={() => setHasLiked(!hasLiked)}>
-                {hasLiked ? '❤️' : '🤍'}
-            </button>
-        </div>
-    )
-}
-
-const App = () => {
-
-    return (
-        <div className="card-container">
-            <Card title="Star Wars" rating={5} isCool={true}/>
-            <Card title="Avatar"/>
-            <Card title="Lion King"/>
-        </div>
-    )
+        <Header/>
+    );
 }
 
 export default App
