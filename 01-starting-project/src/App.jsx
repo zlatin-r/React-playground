3b8551260b5e4add902e477c54a33460856b95cf
zlatin-r <zlatinrusev@gmail.com>
2025-12-21 12:25:52 +0200

    repo cleared

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,19 +1,31 @@
+function Header() {
+    return (
+        <header>
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <h1>React Essentials</h1>
+            <p>
+                Fundamental React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    );
+}
+
+function Main() {
+    return (
+        <main>
+            <h2>Time to get started!</h2>
+        </main>
+    )
+}
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Main/>
+        </div>
+    );
 }
 
 export default App;
