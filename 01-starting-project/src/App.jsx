e524e4b2d41a9068b118306528737ea991b43cc2
zlatin-r <zlatinrusev@gmail.com>
2025-12-21 12:39:32 +0200

    repo cleared

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,29 +1,32 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+    const description = reactDescriptions[genRandomInt(2)];
+
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are
-                going to build!
+                {description} React concepts you will need for almost any app you are going to build!
             </p>
         </header>
     );
-}
-
-function Main() {
-    return (
-        <main>
-            <h2>Time to get started!</h2>
-        </main>
-    )
 }
 
 function App() {
     return (
         <div>
             <Header/>
-            <Main/>
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
         </div>
     );
 }
