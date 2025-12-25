3c81bcb31d4744839318be79a5afde452487d6eb
zlatin-r <zlatinrusev@gmail.com>
2025-12-25 11:26:12 +0200

    commit

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,15 @@
+import {useState} from 'react';
+
 import {CORE_CONCEPTS} from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
-    function handleSelect(selectedButton) {
+    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
 
+    function handleSelect(selectedButton) {
+        setSelectedTopic(selectedButton);
     }
 
     return (
@@ -28,12 +32,12 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onSelect={() => handleSelect()}>Components</TabButton>
-                        <TabButton onSelect={handleSelect}>JSX</TabButton>
-                        <TabButton onSelect={handleSelect}>Props</TabButton>
-                        <TabButton onSelect={handleSelect}>State</TabButton>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
-
+                    {selectedTopic}
                 </section>
             </main>
         </div>
