827d2cc08f257922847c4e1767fd987ff3ea2aaa
zlatin-r <zlatinrusev@gmail.com>
2025-12-25 12:02:05 +0200

    commit

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -13,6 +13,22 @@ function App() {
         setSelectedTopic(selectedButton);
     }
 
+    let tabContent = <p>Please select a topic.</p>
+
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>
+                        {EXAMPLES[selectedTopic].code}
+                    </code>
+                </pre>
+            </div>
+        );
+    }
+
     return (
         <div>
             <Header/>
@@ -38,16 +54,7 @@ function App() {
                         <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                         <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
-                    <div id="tab-content">
-                        <p>Please select a topic.</p>
-                        <h3>{EXAMPLES[selectedTopic].title}</h3>
-                        <p>{EXAMPLES[selectedTopic].description}</p>
-                        <pre>
-                            <code>
-                                {EXAMPLES[selectedTopic].code}
-                            </code>
-                        </pre>
-                    </div>
+                    {tabContent}
                 </section>
             </main>
         </div>
