9b7ba29c1facea75b6cfbfe50e6b1d8a5f08d950
zlatin-r <zlatinrusev@gmail.com>
2025-12-25 11:02:05 +0200

    commit

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,6 +4,10 @@ import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+    function handleSelect(selectedButton) {
+
+    }
+
     return (
         <div>
             <Header/>
@@ -24,11 +28,12 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
+                        <TabButton onSelect={() => handleSelect()}>Components</TabButton>
+                        <TabButton onSelect={handleSelect}>JSX</TabButton>
+                        <TabButton onSelect={handleSelect}>Props</TabButton>
+                        <TabButton onSelect={handleSelect}>State</TabButton>
                     </menu>
+
                 </section>
             </main>
         </div>
