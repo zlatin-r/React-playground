592b0077c3b1fdaeaea9933923bb6e0b9205ee36
zlatin-r <zlatinrusev@gmail.com>
2025-12-26 09:57:45 +0200

    commit

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     }
 
     return (
-        <div>
+        <>
             <Header/>
             <main>
                 <section id="core-concepts">
@@ -64,7 +64,7 @@ function App() {
                     {tabContent}
                 </section>
             </main>
-        </div>
+        </>
     );
 }
 
