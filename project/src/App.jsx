4f51313df291066fe928e72f5af2c16debee65f5
zlatin-r <zlatinrusev@gmail.com>
2025-12-29 06:46:26 +0200

    commit

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css'
+import Header from './components/Header/Header';
 
 function MainGoal() {
     return (
@@ -12,27 +13,7 @@ function MainGoal() {
 function App() {
     return (
         <div id="app">
-            <h1>Time to Practice!</h1>
-            <p>
-                Build a <code>&lt;MainGoal&gt;</code> component and insert it below this
-                text.
-            </p>
-            <p>
-                Your <code>&lt;MainGoal&gt;</code> component should simply output some
-                text that describes your main course goal (e.g., &quot;My main goal:
-                Learn React in great detail&quot;).
-            </p>
-            <p>
-                <strong>Important:</strong> You custom component must contain the text
-                &quot;My main goal:&quot;
-            </p>
-            <p>
-                <strong>Also important:</strong> For the automatic checks to succeed,
-                you <strong>must export</strong> your custom component function! Not as
-                a default but simply by adding the <code>export</code> keyword in front
-                of your function (e.g., <code>export function YOUR_COMPONENT_NAME</code>
-                ).
-            </p>
+            <Header/>
             <MainGoal/>
         </div>
     );
---
+++ project/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <>
+            <header className="header">
+                <h1>Time to Practice!</h1>
+                <p className="subtitle">
+                    Build React components and master the fundamentals
+                </p>
+            </header>
+        </>
+    )
+}
