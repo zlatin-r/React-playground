ca51a165aaa38af5b6a0cf0d2f189d2844675179
zlatin-r <zlatinrusev@gmail.com>
2025-12-29 07:09:44 +0200

    commit

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css'
 import Header from './components/Header/Header';
 import MainGoal from "./components/MainGoal/MainGoal.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
     return (
         <div id="app">
             <Header/>
             <MainGoal/>
+            <menu>
+                <TabButton>JSX</TabButton>
+            </menu>
         </div>
     );
 }
---
+++ project/src/components/TabButton/TabButton.jsx
@@ -0,0 +1,11 @@
+import './TabButton.css'
+
+export default function TabButton({children}) {
+    return (
+        <>
+            <li>
+                <button>{children}</button>
+            </li>
+        </>
+    )
+}
