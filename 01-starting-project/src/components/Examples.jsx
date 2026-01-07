77fb04c1f478c0338d8620040e09a50ae7c157ef
zlatin-r <zlatinrusev@gmail.com>
2026-01-07 07:28:19 +0200

    commit

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,4 +1,5 @@
 import TabButton from "./TabButton";
+import Section from "./Section";
 import {useState} from "react";
 import {EXAMPLES} from "../data";
 
@@ -26,20 +27,19 @@ export default function Examples() {
     }
 
     return (
-        <section id="examples">
-            <h2>Examples</h2>
+        <Section title="Examples" id="examples">
             <menu>
                 <TabButton isSelected={selectedTopic === 'components'}
-                           onSelect={() => handleSelect('components')}>Components</TabButton>
+                           onClick={() => handleSelect('components')}>Components</TabButton>
                 <TabButton isSelected={selectedTopic === 'jsx'}
-                           onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                           onClick={() => handleSelect('jsx')}>JSX</TabButton>
                 <TabButton isSelected={selectedTopic === 'props'}
-                           onSelect={() => handleSelect('props')}>Props</TabButton>
+                           onClick={() => handleSelect('props')}>Props</TabButton>
                 <TabButton isSelected={selectedTopic === 'state'}
-                           onSelect={() => handleSelect('state')}>State</TabButton>
+                           onClick={() => handleSelect('state')}>State</TabButton>
             </menu>
             {tabContent}
-        </section>
+        </Section>
     );
 }
 
