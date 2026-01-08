4b50e434d8ae405b92ce632b592d347683907cc6
zlatin-r <zlatinrusev@gmail.com>
2026-01-08 07:04:25 +0200

    commit

--- project/src/components/Footer/Footer.jsx
+++ project/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 export default function Footer() {
     return (
         <footer>
-            <h1>This is the Footer</h1>
+            <h3>This is the Footer</h3>
         </footer>
     );
 }
