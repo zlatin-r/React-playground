af0a910a4386f27d61c70b9e7f5489a9d521b7e3
zlatin-r <zlatinrusev@gmail.com>
2026-01-09 07:03:16 +0200

    commit

--- project/src/components/Tabs/Tabs.jsx
+++ project/src/components/Tabs/Tabs.jsx
@@ -4,5 +4,5 @@ export default function Tabs({children, buttons, ButtonsContainer}) {
             <ButtonsContainer>{buttons}</ButtonsContainer>
             {children}
         </>
-    )
+    );
 }
