a0b0b0307558f29a61aca90704e88ce21983d031
zlatin-r <zlatinrusev@gmail.com>
2026-01-13 06:50:34 +0200

    commit

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,23 @@
-
 function App() {
-  
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <li>
+                        <span className="player-name">Player 1</span>
+                        <span className="player-symbol">X</span>
+                    </li>
+                    <li>
+                        <span className="player-name">Player 2</span>
+                        <span className="player-symbol">O</span>
+                    </li>
+                </ol>
+                GAME BOARD
+            </div>
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+            LOG
+        </main>
+    )
 }
 
 export default App
