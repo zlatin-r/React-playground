983df47325174ec8f90997029b488ef2d1df8ea0
zlatin-r <zlatinrusev@gmail.com>
2026-01-14 07:06:49 +0200

    commit

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,23 +1,21 @@
-import {useState} from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSelectSquare(rowIndex, colIndex,) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-
-        onSelectSquare();
-    }
+export default function GameBoard({onSelectSquare}) {
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    //
+    // function handleSelectSquare(rowIndex, colIndex,) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id="game-board">
@@ -25,7 +23,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
                 <ol>
                     {row.map((playerSymbol, colIndex) =>
                         <li key={colIndex}>
-                            <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={onSelectSquare}>{playerSymbol}</button>
                         </li>)}
                 </ol>
             </li>)}
