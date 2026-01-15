20028e2c1997256cdabe0eb8c9f6c44fa6a79e53
zlatin-r <zlatinrusev@gmail.com>
2026-01-15 06:41:42 +0200

    commit

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -32,7 +32,10 @@ export default function GameBoard({onSelectSquare, turns}) {
                 <ol>
                     {row.map((playerSymbol, colIndex) =>
                         <li key={colIndex}>
-                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}>
+                                {playerSymbol}
+                            </button>
                         </li>)}
                 </ol>
             </li>)}
