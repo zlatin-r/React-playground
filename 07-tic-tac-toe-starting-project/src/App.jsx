8dd0b7ccebc17e50aec1b1a784d492fcf945c553
zlatin-r <zlatinrusev@gmail.com>
2026-01-16 07:09:32 +0200

    commit

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 import Player from './components/Player';
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import GameOver from "./components/GameOver.jsx";
 import {WINNING_COMBINATIONS} from "./winning-combinations.js";
 
 const initialGameBoard = [
@@ -39,9 +40,9 @@ function App() {
     let winner;
 
     for (const combination of WINNING_COMBINATIONS) {
-        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
-        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].col];
-        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
         if (
             firstSquareSymbol &&
@@ -50,6 +51,7 @@ function App() {
         ) {
             winner = firstSquareSymbol;
         }
+        console.log(firstSquareSymbol, secondSquareSymbol, thirdSquareSymbol);
     }
 
     function handleSelectSquare(rowIndex, colIndex) {
@@ -72,7 +74,7 @@ function App() {
                     <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
                     <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
                 </ol>
-                {winner && <p>You won, {winner}!</p>}
+                {winner && <GameOver winner={winner}/>}
                 <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
             </div>
             <Log turns={gameTurns}/>
